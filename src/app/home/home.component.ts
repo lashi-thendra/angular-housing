f0e05e024ea13e3381c5903b120b59c3799318ab
Lashi Thendra <lashithendrabandara@gmail.com>
2023-08-20 00:23:49 +0530

    add http communication

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,9 +34,11 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
 
-  constructor(){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
   filterResults(text:string){
